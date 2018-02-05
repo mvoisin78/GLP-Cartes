c1e91764112f1e6c7ddcafae1a899a8e80ee7da3
mvoisin78 <mvoisin@hotmail.fr>
2018-02-05 16:19:51 +0100

    05/02

---
+++ GLP_Card/src/player/Score.java
@@ -0,0 +1,30 @@
+package player;
+
+public class Score {
+	private final static int BASELINE_SCORE = 100;
+	private final static int MIN_SCORE = 0;
+	private int score;
+	
+	public Score () {
+		score = BASELINE_SCORE;
+	}
+	
+	public void IncrementerScore (int x) {
+		score += x;
+	}
+	
+	public void DecrementerScore (int x) {
+		if(score - x > MIN_SCORE) {
+			score -= x;
+		}
+		else {
+			score = 0;
+			System.out.println("Le joueur n'as plus de points.");
+		}
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+}
