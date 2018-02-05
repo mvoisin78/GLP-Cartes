0ca6bb6324f036827bf5c548219a31dc009eda4f
HassanPC <HassanPC@192.168.1.19>
2018-02-05 22:04:43 +0100

    Slight modifications

--- GLP_Card/src/card/CardDeck.java
+++ GLP_Card/src/card/CardDeck.java
@@ -2,13 +2,13 @@ package card;
 
 public class CardDeck {
 	private Card[] deck;
-	private static final int CARD_NUMBER = 54;
+	private static final int TOTAL_CARDS= 54;
 	
 	public CardDeck(){
-		deck = new Card[CARD_NUMBER];
+		deck = new Card[TOTAL_CARDS];
 	}
 	
-	public void InitPackage() { //Set the Values and Type for each card in the deck
+	public void InitPackage() { //Sets the Values and Types for each card in the deck
 		int cardNumber = 0;
 		for (EnumColor color : EnumColor.values()) {
 			for(EnumValue value : EnumValue.values()) {
@@ -19,7 +19,8 @@ public class CardDeck {
 			}
 			
 		}
-		deck[52] = new Card(EnumColor.JOKER,EnumValue.JOKER); //init Jokers
+		//init Jokers
+		deck[52] = new Card(EnumColor.JOKER,EnumValue.JOKER);
 		deck[53] = new Card(EnumColor.JOKER,EnumValue.JOKER);
 		
 		
@@ -35,5 +36,15 @@ public class CardDeck {
 	
 	public EnumColor getCardColor(Card card) {
 		return card.getColor();
+	}
+	
+	//Shuffles the cards present in the deck
+	public void shuffle() {
+		for (int i = deck.length-1 ; i>0 ; i--) {
+			int rand = (int)(Math.random()*(i+1));
+			Card temp = deck[i];
+			deck[i]=deck[rand];
+			deck[rand]=temp;
+		}
 	}
 }
