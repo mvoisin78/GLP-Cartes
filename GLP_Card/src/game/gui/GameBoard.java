122cdbcb78e90c8e0331ab0663ad0aa209c751d0
mvoisin78 <mvoisin@hotmail.fr>
2018-02-20 13:33:00 +0100

    added name to the game

--- GLP_Card/src/game/gui/GameBoard.java
+++ GLP_Card/src/game/gui/GameBoard.java
@@ -26,6 +26,7 @@ public class GameBoard {
 			public void run() {
 				try {
 					GameBoard window = new GameBoard();
+					window.frame.setTitle("Tu n'y peux rien ! ");
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
