5f45613ba0eff7ddd0d7433c189d9db536dba772
mvoisin78 <mvoisin@hotmail.fr>
2018-02-28 15:23:45 +0100

    Added detectGameMode() method to detect the mode currently played

--- GLP_Card/src/game/mode/Game.java
+++ GLP_Card/src/game/mode/Game.java
@@ -26,4 +26,55 @@ public class Game {
 			}
 		}
 	}
+	
+	public int detectGameMode(ArrayList<Card> playedCards) {
+		switch(playedCards.size()) {
+		case 1:
+			if(playedCards.get(0).getValue() != EnumValue.JOKER) {
+				return 0; // 0 => Simple
+			}
+			break;
+			
+		case 2:
+			if(playedCards.get(0)==playedCards.get(1)) {
+				return 1; // 1 => Double
+			}
+			break;	
+			
+		case 3:
+			if(playedCards.get(0)==playedCards.get(1) && playedCards.get(0)==playedCards.get(2)) {
+				return 3; // 3 => Triple game
+			}
+			else if(playedCards.get(0).getValue() == EnumValue.JOKER && (playedCards.get(1) == playedCards.get(2))) { // If first Card is Joker
+				return 3;
+			}
+			else if(playedCards.get(1).getValue() == EnumValue.JOKER && (playedCards.get(0) == playedCards.get(2))) { // If Second Card is Joker
+				return 3; 
+			}
+			else if(playedCards.get(2).getValue() == EnumValue.JOKER && (playedCards.get(0) == playedCards.get(1))) { // If Third Card is Joker
+				return 3; 
+			}
+			else {
+				int fCard = playedCards.get(0).getValue().getEnumValue(); //First Card Value
+				int sCard = playedCards.get(1).getValue().getEnumValue(); //Second Card Value
+				int tCard = playedCards.get(2).getValue().getEnumValue(); // Third Card Value
+				if(fCard + 1 == sCard && sCard + 1 == tCard) {
+					return 4; // 4 => Serie of 3 Card
+				}
+				else if(fCard == 17 && (sCard + 1 == tCard)) { // If First Card is Joker
+					return 4;
+				}
+				else if(sCard == 17 && (fCard + 2 == tCard)) { // If Second Card is Joker
+					return 4;
+				}
+				else if(tCard == 17 && (fCard + 1 == sCard)) { // If third Card is Joker
+					return 4;
+				}
+			}
+			break;
+		default : 
+			// Mettre ici un msg d'erreur pour dire au joueur de rejouer dans l'interface
+		}
+		return 666;
+	}
 }
