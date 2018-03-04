cb34d0a17fc4fb3e73b00c51b0250ef11c0f6a77
mvoisin78 <mvoisin@hotmail.fr>
2018-03-04 14:45:46 +0100

    Added 4 et 5 card set

--- GLP_Card/src/game/mode/Game.java
+++ GLP_Card/src/game/mode/Game.java
@@ -28,6 +28,11 @@ public class Game {
 	}
 	
 	public int detectGameMode(ArrayList<Card> playedCards) {
+		int fCard;
+		int sCard;
+		int tCard;
+		int foCard;
+		int fiCard;
 		switch(playedCards.size()) {
 			case 1:
 				if(playedCards.get(0).getValue() != EnumValue.JOKER) {
@@ -55,21 +60,67 @@ public class Game {
 					return 3; 
 				}
 				else {
-					int fCard = playedCards.get(0).getValue().getEnumValue(); //First card value
-					int sCard = playedCards.get(1).getValue().getEnumValue(); //Second card value
-					int tCard = playedCards.get(2).getValue().getEnumValue(); // Third card value
+					fCard = playedCards.get(0).getValue().getEnumValue(); //First card value
+					sCard = playedCards.get(1).getValue().getEnumValue(); //Second card value
+					tCard = playedCards.get(2).getValue().getEnumValue(); // Third card value
 					if(fCard + 1 == sCard && sCard + 1 == tCard) {
 						return 4; // 4 => Set of 3 cards
 					}
-					else if(fCard == 17 && (sCard + 1 == tCard)) { // If first card is a Joker
+					else if(fCard == 17 && (sCard + 1 == tCard)) { // If the first card is a Joker
 						return 4;
 					}
-					else if(sCard == 17 && (fCard + 2 == tCard)) { // If second card is a Joker
+					else if(sCard == 17 && (fCard + 2 == tCard)) { // If the second card is a Joker
 						return 4;
 					}
-					else if(tCard == 17 && (fCard + 1 == sCard)) { // If third card is a Joker
+					else if(tCard == 17 && (fCard + 1 == sCard)) { // If the third card is a Joker
 						return 4;
 					}
+				}
+				break;
+			case 4:
+				fCard = playedCards.get(0).getValue().getEnumValue(); //First card value
+				sCard = playedCards.get(1).getValue().getEnumValue(); //Second card value
+				tCard = playedCards.get(2).getValue().getEnumValue(); // Third card value
+				foCard = playedCards.get(3).getValue().getEnumValue(); // Fourth card value
+				if(fCard + 1 == sCard && sCard + 1 == tCard && tCard + 1 == foCard) {
+					return 5; // 5 => Set of 4 cards
+				}
+				else if(fCard == 17 && (sCard + 1 == tCard) && (tCard +1 == foCard)) { // If the first card is a Joker
+					return 5;
+				}
+				else if(sCard == 17 && (fCard + 2 == tCard) && (tCard +1 == foCard)) { // If the second card is a Joker
+					return 5;
+				}
+				else if(tCard == 17 && (fCard + 1 == sCard) && (sCard +2 == foCard)) { // If the third card is a Joker
+					return 5;
+				}
+				else if(foCard == 17 && (fCard + 1 == sCard) && (sCard +1 == tCard)) { // If the fourth card is a Joker
+					return 5;
+				}
+				break;
+			case 5:
+				fCard = playedCards.get(0).getValue().getEnumValue(); //First card value
+				sCard = playedCards.get(1).getValue().getEnumValue(); //Second card value
+				tCard = playedCards.get(2).getValue().getEnumValue(); // Third card value
+				foCard = playedCards.get(3).getValue().getEnumValue(); // Fourth card value
+				fiCard = playedCards.get(4).getValue().getEnumValue(); // Fifth card value
+				if(fCard + 1 == sCard && sCard + 1 == tCard && tCard + 1 == foCard && foCard +1 == fiCard) {
+					return 5; // 5 => Set of 4 cards
+				}
+				else if(fCard == 17 && (sCard + 1 == tCard) && (tCard +1 == foCard) && (foCard + 1 == fiCard)) { // If the first card is a Joker
+					return 5;
+				}
+				else if(sCard == 17 && (fCard + 2 == tCard) && (tCard +1 == foCard) && (foCard + 1 == fiCard)) { // If the second card is a Joker
+					return 5;
+				}
+				else if(tCard == 17 && (fCard + 1 == sCard) && (sCard +2 == foCard) && (foCard + 1 == fiCard)) { // If the third card is a Joker
+					return 5;
+				}
+				else if(foCard == 17 && (fCard + 1 == sCard) && (sCard +1 == tCard) && (tCard + 2 == fiCard)) { // If the fourth card is a Joker
+					return 5;
+				}
+				else if(fiCard == 17 && (fCard + 1 == sCard) && (sCard +1 == tCard) && (tCard + 1 == foCard)) { // If the fifth card is a Joker
+					return 5;
 				}
 				break;
 			default: 
