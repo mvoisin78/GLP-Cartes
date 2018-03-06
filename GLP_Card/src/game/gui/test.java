2f82c56bed8c5682592a6bc43e714fef79cc496f
hassight <sisalem.hassan@gmail.com>
2018-03-06 15:48:55 +0100

    gg

--- GLP_Card/src/game/gui/test.java
+++ GLP_Card/src/game/gui/test.java
@@ -3,6 +3,7 @@ package game.gui;
 import java.awt.Color;
 import java.awt.EventQueue;
 
+import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -10,11 +11,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.SoftBevelBorder;
 
 public class test extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	private JFrame frame = new JFrame();
+	private JPanel center = new JPanel();
 	private JPanel bottom = new JPanel();
 	private JPanel aside = new JPanel();
 	private JButton jouer = new JButton("Jouer");
@@ -37,47 +42,50 @@ public class test extends JPanel{
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		frame.getContentPane().setBackground(new Color(0, 128, 0));
-		
-		bottom.setBounds(0, 580, 900, 200);
+		center.setLayout(null);
+		center.setBackground(new Color(0, 128, 0));
+				
+		bottom.setBounds(0, 590, 862, 82);
+//		bottom.setBounds(x, y, width, height);
+		bottom.setBorder(new BevelBorder(BevelBorder.RAISED));
 		frame.add(bottom);
-		
 		
 		bottom.add(jouer);
 		
-		aside.setBounds(900, 0, 220, 700);
+		aside.setBounds(860, 0, 260, 700);
+		aside.setBorder((Border) new SoftBevelBorder(SoftBevelBorder.LOWERED));
 		frame.add(aside);
 		
 		aside.add(score);
 		
 		Icon handCard = new ImageIcon("../resources/images/clubs0.gif");
 		JLabel hand = new JLabel(handCard);
-		hand.setBounds(100, 55, 100, 150);
-		frame.getContentPane().add(hand);
+		hand.setBounds(80, 55, 100, 150);
+		center.add(hand);
 		
         Icon handCard2 = new ImageIcon("../resources/images/hearts0.gif");
         ButtonLabel label = new ButtonLabel(handCard2);
-        label.setBounds(440, 55, 103, 150);
-		frame.getContentPane().add(label);
+        label.setBounds(420, 55, 103, 150);
+		center.add(label);
 		//
         Icon handCard3 = new ImageIcon("../resources/images/spades0.gif");
         ButtonLabel label2 = new ButtonLabel(handCard3);
-        label2.setBounds(780, 55, 100, 150);
-		frame.getContentPane().add(label2);
+        label2.setBounds(760, 55, 100, 150);
+		center.add(label2);
 		
 		JLabel label3 = new JLabel("");
-		label3.setBounds(440, 395, 100, 150);
+		label3.setBounds(420, 395, 100, 150);
         Icon handCard4 = new ImageIcon("../resources/images/diamonds0.gif");
         label3.setIcon(handCard4);
-		frame.getContentPane().add(label3);
+		center.add(label3);
 		
 		JLabel deck = new JLabel("");
-		deck.setBounds(780, 395, 100, 150);
+		deck.setBounds(760, 395, 100, 150);
         Icon cover = new ImageIcon("../resources/images/cover.gif");
         deck.setIcon(cover);
-		frame.getContentPane().add(deck);
+		center.add(deck);
 		
+		frame.add(center);
 		frame.setVisible(true);
 	}
     
