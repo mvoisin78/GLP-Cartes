7736595389c4a9c1c75fb513ab10b26d972ed895
tparrine <thparr5@gmail.com>
2018-03-06 14:13:24 +0100

    game board

--- GLP_Card/src/game/card/Card.java
+++ GLP_Card/src/game/card/Card.java
@@ -4,10 +4,13 @@ public class Card {
 	//private String type; 
 	private EnumValue value;   //Value of the card (From 3 to Ace, then Joker and 2)
 	private EnumColor color;  //Clubs - Diamonds - Hearts - Spades
+	private String pathImage;
 	
 	public Card(EnumColor color, EnumValue value) { 
 		this.color = color;
 		this.value = value;
+		
+		pathImage = "../resources/images/"+value+"_"+color+".png";
 	}
 
 	public EnumColor getColor() {
--- GLP_Card/src/game/gui/test.java
+++ GLP_Card/src/game/gui/test.java
@@ -2,8 +2,10 @@ package game.gui;
 
 import java.awt.Color;
 import java.awt.EventQueue;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -26,17 +28,28 @@ public class test extends JPanel{
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setSize(1000, 700);
+		frame.setSize(1100, 700);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().setBackground(new Color(0, 128, 0));
 		
-		JLabel hand = new JLabel("");
+		JPanel bottom = new JPanel();
+		bottom.setBackground(Color.white);
+		bottom.setBounds(0, 580, 900, 200);
+		frame.add(bottom);
+		
+		JButton jouer = new JButton("jouer");
+		bottom.add(jouer);
+		
+		JPanel aside = new JPanel();
+		aside.setBackground(Color.white);
+		aside.setBounds(900, 0, 120, 700);
+		frame.add(aside);
+		
+		Icon handCard = new ImageIcon("../resources/images/clubs0.gif");
+		JLabel hand = new JLabel(handCard);
 		hand.setBounds(100, 55, 100, 150);
-        Icon handCard = new ImageIcon("../resources/images/clubs0.gif");
-        hand.setIcon(handCard);
-        hand.setEnabled(true);
 		frame.getContentPane().add(hand);
 		
         Icon handCard2 = new ImageIcon("../resources/images/hearts0.gif");
@@ -60,6 +73,8 @@ public class test extends JPanel{
         Icon cover = new ImageIcon("../resources/images/cover.gif");
         deck.setIcon(cover);
 		frame.getContentPane().add(deck);
+		
+		
 	}
     
     /**
