2fe7260a07bc661fb66c5bfde4f22ab62f5ec78c
Théo <Tho@192.168.1.13>
2018-03-09 16:15:49 +0100

    Added attribut "selectedCard" to the class "player"

--- GLP_Card/src/game/player/Player.java
+++ GLP_Card/src/game/player/Player.java
@@ -1,27 +1,42 @@
 package game.player;
 
+import java.util.ArrayList;
+
 import game.card.*;
 
 public abstract class Player {
-	private Score score;
-	private String name;
-	private Hand hand;
-	
-	public Player(String name) {
-		hand = new Hand();
-		score = new Score();
-		this.name = name;
-	}
-	
-	public Hand getHand() {
-		return hand;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public Score getScore() {
-		return score;
-	}
+    private Score score;
+    private String name;
+    private Hand hand;
+    private ArrayList<Card> selectedCards; //Cards selected by the player to compar with the history to verify if he can play this card
+    
+    public Player(String name) {
+        hand = new Hand();
+        score = new Score();
+        this.name = name;
+    }
+    
+    public Hand getHand() {
+        return hand;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Score getScore() {
+        return score;
+    }
+    
+    public void addSelectedCard(Card card) {
+        selectedCards.add(card);
+    }
+    
+    public void supSelectedCard(Card card) {
+    	selectedCards.remove(card);
+    }
+    
+    public void resetSelectedCard() {
+        selectedCards.clear();
+    }
 }
